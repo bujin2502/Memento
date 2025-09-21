c00ef2ebc51f623d2759886b4fae3891d26935f3
zlatko <zlatkopr@gmail.com>
2025-09-21 12:34:22 +0200

    005 - dovršetak 5. vježbe

--- app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
@@ -5,13 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.adapters.TasksAdapter
+import hr.foi.rampu.memento.helpers.MockDataLoader
+
 
 class PendingFragment : Fragment() {
+    private val mockTasks = MockDataLoader.getDemoData()
+    private lateinit var recyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // mockTasks.forEach { Log.i("MOCK_PENDING_TASKS", it.name) }
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_pending_tasks)
+        recyclerView.adapter = TasksAdapter(MockDataLoader.getDemoData())
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
     }
 }
